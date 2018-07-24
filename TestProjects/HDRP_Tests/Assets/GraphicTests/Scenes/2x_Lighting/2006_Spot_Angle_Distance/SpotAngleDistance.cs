4ca2a9e606a641b54e6b24f6bddcaf21ba19a7fb
Remy <remym@unity3d.com>
2018-07-24 09:15:44 +0200

    Add spot angle test

---
+++ TestProjects/HDRP_Tests/Assets/GraphicTests/Scenes/2x_Lighting/2006_Spot_Angle_Distance/SpotAngleDistance.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.HDPipeline;
+
+[RequireComponent(typeof(Light))]
+public class SpotAngleDistance : MonoBehaviour 
+{
+	public float targetRadius = 1f;
+	public float referenceIntensity = 600f;
+	public float referenceDistance = 1.5f;
+
+	public enum Mode {Distance, Angle}
+	public Mode mode = Mode.Distance;
+	public float distance = 3f;
+	public float angle = 60f;
+
+	[SerializeField, HideInInspector] private Light light;
+	[SerializeField, HideInInspector] private HDAdditionalLightData hdLightData;
+
+	void OnValidate()
+	{
+		if (light == null) light = GetComponent<Light>();
+		if (light == null) return;
+		if (hdLightData == null) hdLightData = GetComponent<HDAdditionalLightData>();
+		if (hdLightData == null) return;
+
+		if ( mode == Mode.Distance)
+		{
+			float t = targetRadius / distance;
+			angle = Mathf.Atan(t) * Mathf.Rad2Deg * 2f;
+		}
+		else
+		{
+			float t = Mathf.Tan(Mathf.Deg2Rad * angle * 0.5f);
+			distance = targetRadius / t;
+		}
+
+		light.spotAngle = angle;
+		light.range = distance + 10f;
+		transform.localPosition = -distance * Vector3.forward;
+
+		hdLightData.intensity = referenceIntensity * Mathf.Pow( distance / referenceDistance , 2f );
+	}
+}
