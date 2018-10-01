f4549597299c7c14e470ba13d3da4709225a621e
Nerites <paula.s.ning@gmail.com>
2018-10-01 13:56:55 -0700

    Added bool to control override of XRSettings by SRP (off by default)

--- com.unity.render-pipelines.core/Editor/XRGraphicsConfigDrawer.cs
+++ com.unity.render-pipelines.core/Editor/XRGraphicsConfigDrawer.cs
@@ -5,14 +5,17 @@ namespace UnityEngine.Experimental.Rendering
     [CustomPropertyDrawer(typeof(XRGraphicsConfig))]
     public class XRGraphicsConfigDrawer : PropertyDrawer
     {
+        public AnimBool srpOverride; 
         internal class Styles
         {
-            public static GUIContent XRSettingsLabel = new GUIContent("XR Config", "Enable XR in Player Settings. Then SetConfig can be used to set this configuration to XRSettings.");
+            public static GUIContent XRSettingsLabel = new GUIContent("XR Config", "Enable XR in Player Settings, then enable SRP Override of XRSettings. Then the below values will be set to XRSettings by SRP.");
             public static GUIContent srpOverrideLabel = new GUIContent("Override XRSettings with SRP", "Overwrite default and non-SRP-programmed XRSettings with the values chosen in this SRP asset.");
             public static GUIContent useOcclusionMeshLabel = new GUIContent("Use Occlusion Mesh", "Determines whether or not to draw the occlusion mesh (goggles-shaped overlay) when rendering");
             public static GUIContent occlusionScaleLabel = new GUIContent("Occlusion Mesh Scale", "Scales the occlusion mesh");
+            public static GUIContent warnVRDisabled = EditorGUIUtility.TrTextContent("VR is disabled in Player Settings.");
 
         }
+
         // Draw the property inside the given rect
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
@@ -22,13 +25,20 @@ namespace UnityEngine.Experimental.Rendering
 
             EditorGUILayout.LabelField(Styles.XRSettingsLabel, EditorStyles.boldLabel);
             EditorGUILayout.PropertyField(drawSRPOverride, Styles.srpOverrideLabel);
-            EditorGUI.BeginDisabledGroup(!XRGraphicsConfig.tryEnable);
-            EditorGUI.indentLevel++;
-            EditorGUILayout.PropertyField(drawUseOcclusionMesh, Styles.useOcclusionMeshLabel);
-            EditorGUILayout.PropertyField(drawOcclusionMeshScale, Styles.occlusionScaleLabel);
-            EditorGUI.indentLevel--;
-            EditorGUILayout.Space();
-            EditorGUI.EndDisabledGroup();
+            if (XRGraphicsConfig.tryEnable)
+            {
+                EditorGUI.BeginDisabledGroup(!drawSRPOverride.boolValue);
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(drawUseOcclusionMesh, Styles.useOcclusionMeshLabel);
+                EditorGUILayout.PropertyField(drawOcclusionMeshScale, Styles.occlusionScaleLabel);
+                EditorGUI.indentLevel--;
+                EditorGUILayout.Space();
+                EditorGUI.EndDisabledGroup();
+            }
+            else
+            {
+                EditorGUILayout.HelpBox(Styles.warnVRDisabled.text, MessageType.Warning);
+            }
         }
     }
 }
