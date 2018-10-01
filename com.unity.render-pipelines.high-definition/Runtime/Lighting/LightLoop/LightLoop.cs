c7cc8f26a61eb385f1d1d7bfc766971d31787433
Evgenii Golubev <evgenii@unity3d.com>
2018-10-01 19:01:08 +0200

    Add a comment

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.cs
@@ -1026,6 +1026,8 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
 
             if (lightData.lightType != GPULightType.Directional && lightData.lightType != GPULightType.ProjectorBox)
             {
+                // Store the squared radius of the light to simulate a fill light.
+                // Also important in order to not divide by 0.
                 lightData.size = new Vector2(additionalLightData.shapeRadius * additionalLightData.shapeRadius, 0);
             }
 
