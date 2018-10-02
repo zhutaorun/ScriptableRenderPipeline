42f51e305c232672240a1e23687d9e11c6e0456c
Raymond Graham <raymond.graham4087@gmail.com>
2018-10-02 15:40:26 -0700

    debug to see the 2D light cookies

--- com.unity.render-pipelines.high-definition/HDRP/RenderPipeline/HDCustomSamplerId.cs
+++ com.unity.render-pipelines.high-definition/HDRP/RenderPipeline/HDCustomSamplerId.cs
@@ -47,6 +47,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         HDRenderPipelineRender,
         CullResultsCull,
         CopyDepthForSceneView,
+        DisplayCookies,
         DownsampleShadowMaps,
 
         // Profile sampler for tile pass
