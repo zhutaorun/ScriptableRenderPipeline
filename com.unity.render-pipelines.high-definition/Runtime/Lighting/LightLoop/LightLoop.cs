73c9bd56714b919b90501c2a4cf9164a8d22ac93
Evgenii Golubev <evgenii@unity3d.com>
2018-10-03 13:33:00 +0200

    Fix the radius clamp

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/LightLoop.cs
@@ -1028,8 +1028,9 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             {
                 // Store the squared radius of the light to simulate a fill light.
                 // Also important in order to not divide by 0.
-                // The slider already clamps, but we clamp again to ensure there is possibility of a NaN.
-                lightData.size = new Vector2(Mathf.Max(additionalLightData.shapeRadius * additionalLightData.shapeRadius, 0.01f), 0);
+                // The slider already clamps, but we clamp again to ensure there is no possibility of a NaN.
+                float radius = Mathf.Max(additionalLightData.shapeRadius, 0.01f);
+                lightData.size = new Vector2(radius * radius, 0);
             }
 
             if (lightData.lightType == GPULightType.Rectangle || lightData.lightType == GPULightType.Line)
